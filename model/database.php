c066b8c7f34335f77ffd83019197ca81ea71e79b
soumitad <sd655@njit.edu>
2017-05-03 23:19:38 -0400

    Update db.phhp

--- model/database.php
+++ model/database.php
@@ -1,8 +1,8 @@
 <?php
 // Set up the database connection
-$dsn = 'mysql:host=localhost;dbname=my_guitar_shop2';
-$username = 'mgs_user';
-$password = 'pa55word';
+$dsn = 'mysql:host=sql.njit.edu;dbname=sd655';
+    $username = 'sd655';
+    $password = 'pSLU9F3d';
 $options = array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION);
 
 try {
